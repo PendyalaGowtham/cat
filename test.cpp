f0f4aee4f402847d1e9c2d8c8741d416e982bea9
Samuel Murari <sammurari17@gmail.com>
2022-01-18 21:22:52 +0530

    com

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	
+	cout<<"Are you hungry?"<<endl;
+	string hungry,yes,no;
+	cin>>hungry;
+	
+	if(hungry=="yes")
+	{
+	cout<<"let have some stacks"<<endl;
+	}
+	else if(hungry=="no"){
+	cout<<"then go to study"<<endl;
+	}
+}
